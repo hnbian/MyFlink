65fd42b0f0accb6f8747463b9a78c945ed7baf1a
hnbian <hnbian@126.com>
2020-07-20 22:32:03 +0800

    分组处理

--- FlinkCode/src/main/scala/com/hnbian/flink/transform/TransformGroup.scala
+++ FlinkCode/src/main/scala/com/hnbian/flink/transform/TransformGroup.scala
@@ -14,14 +14,14 @@ object TransformGroup {
     // 创建执行环境
     val env = StreamExecutionEnvironment.getExecutionEnvironment
 
-    env.fromCollection(List(
+    /*env.fromCollection(List(
       "hello world", "how are you", "how old are you"
     ))
       .flatMap(str => {
         str.split(" ")
       }).map(v=>{(v,1)})
       .keyBy(0)
-      .sum(1).print()
+      .sum(1).print()*/
 
     /**
       * 7> (you,1)
@@ -36,7 +36,29 @@ object TransformGroup {
       */
 
 
+    println("----")
+
     // reduce
+    env.fromCollection(List(
+      "hello world", "how are you", "how old are you"
+    )).flatMap(str => {
+      str.split(" ")
+    }).map(v=>{(v,1)})
+      .keyBy(0)
+      .reduce((a,b)=>(a._1,b._2+a._2))
+        .print("reduce")
+
+    /**
+      * reduce:4> (hello,1)
+      * reduce:6> (are,1)
+      * reduce:11> (old,1)
+      * reduce:9> (how,1)
+      * reduce:7> (world,1)
+      * reduce:9> (how,2)
+      * reduce:7> (you,1)
+      * reduce:6> (are,2)
+      * reduce:7> (you,2)
+      */
 
 
     env.execute()
